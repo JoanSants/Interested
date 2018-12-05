42be97dee64f61e7bda3a0aa5c376710d0991e5a
JoanSants <joansants07@gmail.com>
2018-12-05 11:11:40 -0200

    Navigation Changes

--- src/components/Navigation/Categories/Category/Category.js
+++ src/components/Navigation/Categories/Category/Category.js
@@ -4,7 +4,7 @@ import {NavItem} from 'react-materialize'
 
 const category = props => {
     return(
-        <NavLink to={'/categories/' + props.name}>{props.children}<NavItem divider /></NavLink>
+        <NavLink to={'/categories/' + props.name.toLowerCase()}>{props.children}<NavItem divider /></NavLink>
     )
 }
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { Dropdown, Button, NavItem } from 'react-materialize';
+import {NavLink} from 'react-router-dom';
 
 import NavigationItem from './NavigationItem/NavigationItem';
 import Categories from './../Categories/Categories';
 
 const navigationItems = props => {
+    
+    const accountDropDown = (
+        <Dropdown trigger={
+            <Button>Minha Conta</Button>
+        }>
+            <NavLink to='/my-interests'>Meus Interesses<NavItem divider /></NavLink>
+            <NavLink to='/my-info'>Meus dados<NavItem divider /></NavLink>
+            <NavLink to='/contacts'>Contatos<NavItem divider /></NavLink>
+            <NavLink to='/logout'>Sair<NavItem divider /></NavLink>
+        </Dropdown>
+    )
+
     return (
         <ul>
             <NavigationItem link='/add-interest'>Em comprar</NavigationItem>
-            <NavigationItem link='/add-item'>Em vender</NavigationItem>
-            {!props.isAuth ? 
-                <NavigationItem link='/authenticate'>Entrar</NavigationItem> : 
-                <NavigationItem link='/logout'>Sair</NavigationItem> }
-            <Categories/>
+            <NavigationItem link='/keys'>Chaves</NavigationItem>
+            {!props.isAuth ?
+                <NavigationItem link='/authenticate'>Entrar</NavigationItem> :
+                accountDropDown}
+            <Categories />
         </ul>
     )
 }
